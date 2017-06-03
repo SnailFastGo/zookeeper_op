faa500cc53aa998110e6d3ce47bd17f1b2a39260
SnailFastGo <181223860@qq.com>
2017-06-03 15:44:04 +0800

    以同步的方式读取zookeeper节点数据

--- src/main/java/com/zk/zookeeper/GetDataAsyc.java
+++ src/main/java/com/zk/zookeeper/GetDataAsyc.java
@@ -1,7 +1,5 @@
 package com.zk.zookeeper;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import org.apache.zookeeper.KeeperException;
@@ -12,25 +10,15 @@ import org.apache.zookeeper.Watcher.Event.KeeperState;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
-import com.zk.utils.StaticVar;
-
-/** 以同步的方式读取zookeeper节点数据
+/** 以异步的方式读取zookeeper节点数据
  * @author snailfast
  *
  */
 public class GetDataAsyc implements Watcher{
+	
 	private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
 	private static ZooKeeper zookeeper = null;
 	private static Stat stat = new Stat();
-	
-	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-		zookeeper = new ZooKeeper(StaticVar.connectString, StaticVar.sessionTimeout, new GetDataAsyc());
-		connectedSemaphore.await();
-		String path = "/zk-test-persistent";
-		byte[] data = zookeeper.getData(path, new GetDataAsyc(), stat);
-		System.out.println(new String(data));
-		zookeeper.close();
-	}
 	
 	@Override
 	public void process(WatchedEvent event) {
